beb8077fc606d01ee3a193449c0cda218f41b9be
mianxiu <mianxiu8481@gmail.com>
2017-11-25 17:02:16 +0800

    1

--- public/JavaScript/index.js
+++ public/JavaScript/index.js
@@ -1,74 +1,73 @@
 
 
 window.onload = function () {
-  mp3Player();
+    mp3Player();
 }
 
 
-function r (){
-    var a = ['这是什么', '不会是真的吧', '开个玩乐', '无用功', '惨~~','surprise!!!','........']
-        getRandomInt = function (min, max) {
-            min = Math.ceil(min)
-            max = Math.floor(max)
-            return Math.floor((Math.random() * (max - min)) + min)
-        }
-        document.querySelector('button.k').innerText = a[getRandomInt(1, 5)]
+function r() {
+    var a = ['这是什么', '不会是真的吧', '开个玩乐', '无用功', '惨~~', 'surprise!!!', '........']
+    getRandomInt = function (min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor((Math.random() * (max - min)) + min)
+    }
+    document.querySelector('button.k').innerText = a[getRandomInt(1, 5)]
 }
 
-function getIndex(childNode){
+function getIndex(childNode) {
     //获取索引值
-    let p =  childNode.parentNode
+    let p = childNode.parentNode
     let pChild = p.children
-        for(i=0;i<pChild.length;i++){
-            if(pChild[i] === childNode){
-                  return i
-                }
-            
-            }
+    for (i = 0; i < pChild.length; i++) {
+        if (pChild[i] === childNode) {
+            return i
         }
 
-function mp3Player(){
+    }
+}
+
+function mp3Player() {
     //播放器
-    var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
     var playList = []
     for (const l of document.querySelector('#playList').children) {
-        playList.push('public/music/'+l.innerText+'.mp3') 
+        playList.push('public/music/' + l.innerText + '.mp3')
     }
 
 
     var player = document.querySelector('#player')
-    var source =  audioCtx.createMediaElementSource(player);   
+    var source = audioCtx.createMediaElementSource(player);
 
-  
+
     var analyser = audioCtx.createAnalyser()
-    
+
     source.connect(analyser);
     analyser.connect(audioCtx.destination)
 
 
     //默认音源
-    player.src=playList[0]
-    player.onended = function(){
-            console.log('下一曲')
-            let host =  decodeURI(player.src).replace(/https\:\/\/mianxiu\.github\.io\//,'')
-            console.log(host)
-            let e = playList.indexOf(host)
-            console.log(player.src)
-            console.log(e+' , '+playList.length)
-            if(e+1 <= playList.length){
-                return player.src = playList[e+1]
-              
-            }else{
-                return player.src = playList[0]
-            }
+    //循环播放列表
+    player.src = playList[0]
+    player.onended = function () {
+        console.log('下一曲')
+        let host = decodeURI(player.src).replace(window.location.href, '')
+        let e = playList.indexOf(host)
+        console.log(e + ' , ' + playList.length)
+        if (e + 1 <= playList.length) {
+            return player.src = playList[e + 1]
 
-        
+        } else {
+            return player.src = playList[0]
+        }
+
+
     }
-    document.querySelector('#mp3Player').addEventListener('dblclick',function(db){
-    //根据双击加载音源 
-    player.src = playList[getIndex(db.path[0])]
-    
+    document.querySelector('#mp3Player').addEventListener('dblclick', function (db) {
+        //根据双击加载音源 
+        player.src = playList[getIndex(db.path[0])]
+
     })
-    
+
 }
