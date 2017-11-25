1df758fd390091adac318b576cbff0119c5e8a33
mianxiu <mianxiu8481@gmail.com>
2017-11-25 19:20:33 +0800

    1

--- public/JavaScript/index.js
+++ public/JavaScript/index.js
@@ -76,7 +76,7 @@ function mp3Player() {
         drawVisual = requestAnimationFrame(draw)
         analyser.getByteFrequencyData(dataArray)
 
-        canvasCtx.fillStyle = 'rgb(0, 0, 0)'
+        canvasCtx.fillStyle = 'rgb(0, 0, 0,0)'
         canvasCtx.fillRect(0,0,WIDTH,HEIGHT)
 
         var barWidth = (WIDTH/bufferLength)*2.5
@@ -84,7 +84,7 @@ function mp3Player() {
         
         for(i=0;i<bufferLength;i++){
             barHeight = dataArray[i]
-            canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+            canvasCtx.fillStyle = 'rgba(' + (barHeight+100) + ',50,50,0)';
             canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
         
             x += barWidth + 1;
