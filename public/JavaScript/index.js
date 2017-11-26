5f49443a47801a7125d22232c4966c246aaf3dbf
mianxiu <mianxiu8481@gmail.com>
2017-11-26 20:43:40 +0800

    1

--- public/JavaScript/index.js
+++ public/JavaScript/index.js
@@ -161,7 +161,7 @@ function mp3Player() {
             //绘制            
             for (i = 0; i < bufferLength; i++) {
                 barHeight = dataArray[i] 
-                canvasCtx.fillRect(x,0, 6, barHeight / 2);
+                canvasCtx.fillRect(x,100, 6, barHeight / 2);
                // x += barWidth + 1;
                 x +=7
             }
