59a490eb5916546245d6f4627f79965c8e8be817
mianxiu <mianxiu8481@gmail.com>
2017-12-19 17:48:11 +0800

    beta

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,8 +1,8 @@
 function navHidden(type){
     //移动端进入子栏目后更改导航样式
     if(type === 'on'){
-        $('#navigation').style = 'height:100px;'
-        $('#navigation > ul').style = 'display:flex;flex-direction:row;position:absolute;overflow-x:hidden;'
+        $('#navigation').style = 'height:100px;position:absolute;overflow:hidden;background-color:red;width:100vw;'
+        $('#navigation > ul').style = 'display:flex;flex-direction:row;osition:absolute;'
         $('#essayText').style = 'z-index:4;'
     }else if(type === 'off'){
         $('#navigation').style = ''
