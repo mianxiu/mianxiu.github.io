3b739ee72ea4a7558d750bb326f842f4eac74abf
mianxiu <mianxiu8481@gmail.com>
2017-12-24 11:17:05 +0800

    beta

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,7 +1,7 @@
 function navHidden(type){
     //移动端进入子栏目后更改导航样式
     if(type === 'on'){
-        $('#navigation').style = 'height:100px;position:absolute;overflow:hidden;background-color:red;width:100vw;'
+        $('#navigation').style = 'height:100px;position:absolute;overflow:hidden;width:100vw;'
         $('#navigation > ul').style = 'display:flex;flex-direction:row;position:absolute;'
         $('#essayText').style = 'z-index:4;'
         navTouchMove()
