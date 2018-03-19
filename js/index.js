e76f83faf483e20d69572c55009e61c2e6bffe00
mianxiu <mianxiu8481@gmail.com>
2018-03-19 17:52:26 +0800

    beta

--- js/index.js
+++ js/index.js
@@ -36,7 +36,6 @@ function $All(dom) {
 }
 
 
-
 function r() {
     var a = ['这是什么', '不会是真的吧', '开个玩乐', '无用功', '惨~~', 'surprise!!!', '........']
     getRandomInt = function (min, max) {
@@ -46,8 +45,6 @@ function r() {
     }
     $('button.k').innerText = a[getRandomInt(1, 5)]
 }
-
-
 
 
 /**
@@ -106,7 +103,6 @@ function playListAry() {
     }
     return playList
 }
-
 
 
 
